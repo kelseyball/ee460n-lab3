3997770968214bf5773e894a098f539c62c346fb
Kelsey Ball <kelseytaylorball@gmail.com>
2016-10-16 23:04:18 -0500

    stw test

---
+++ tests/stw.asm
@@ -0,0 +1,22 @@
+        .ORIG x3000
+		lea r4 number
+		ldw r4 r4 #0
+
+		lea r0 data
+		stw r4 r0 #-1
+
+		add r4 r4 #-1
+		stw r4 r0 #0
+
+		add r4 r4 #-1
+		stw r4 r0 #1
+
+		ldw r1 r0 #-1 
+		ldw r2 r0 #0
+		ldw r3 r0 #1
+		halt
+number	.fill x1234
+		.fill #0
+data	.fill #0
+		.fill #0
+        .END
